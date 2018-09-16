5c76f69e6cc58aa0cb6a15554dbd013f70550202
pytf <411016234@.qq.com>
2018-09-16 17:59:13 +0800

    Commit the code to github

---
+++ src/clwf/core/common/type.h
@@ -0,0 +1,16 @@
+
+/*
+ * Define 
+ * 
+ */
+
+#ifndef _TYPE_H_
+#define _TYPE_H_
+
+typedef char lwf_char;
+typedef unsigned char lwf_uchar;
+typedef unsigned int lwf_int16;
+typedef int lwf_int32;
+typedef float lwf_int64;
+
+#endif
