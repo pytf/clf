e90a5351ee5882d92b1b09110850195e0234fab3
pytf <43243160+pytf@users.noreply.github.com>
2018-10-09 17:01:42 +0800

    Update type.h

--- src/clf/core/common/type.h
+++ src/clf/core/common/type.h
@@ -1,6 +1,6 @@
 
 /*
- * Define basic types for lwf
+ * Define basic types for lf
  * 
  */
 
@@ -11,22 +11,15 @@
 #if __cplusplus
 extern "C"{
 #endif
-#endif
+#endif /* __cplusplus */
 
-typedef signed char s8;
-typedef unsigned char u8;
+typedef unsigned char unchar;
 
-typedef signed short s16;
-typedef unsigned short u16;
+typedef unsigned short unshort;
 
-typedef signed int s32;
-typedef unsigned int u32;
+typedef unsigned int unint;
 
-typedef signed long long s64;
-typedef unsigned long long u64;
-
-typedef void ;
-typedef void* pvoid;
+typedef unsigned long long unlong;
 
 #define RET_OK (0)
 #define RET_ERR (-1)
@@ -35,6 +28,7 @@ typedef void* pvoid;
 #if __cplusplus
 }
 #endif
-#endif
+#endif /* __cplusplus */
 
-#endif
+#endif /* _TYPE_H_ */
+
