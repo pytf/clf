efd61acf7f98f083fff3f47c21efb03fbae051c3
pytf <43243160+pytf@users.noreply.github.com>
2018-10-09 17:00:21 +0800

    Update lf_time.h

--- src/clf/os/unix/lf_time.h
+++ src/clf/os/unix/lf_time.h
@@ -1,22 +1,7 @@
+#ifndef _lf_TIME_H_
+#define _lf_TIME_H_
 
-/*
- * Copyright (C) Igor Sysoev
- * Copyright (C) Nginx, Inc.
- */
-
-
-#ifndef _lf_TIME_H_INCLUDED_
-#define _lf_TIME_H_INCLUDED_
-
-
-#include <lf_config.h>
-#include <lf_core.h>
-
-
-typedef lf_rbtree_key_t      lf_msec_t;
-typedef lf_rbtree_key_int_t  lf_msec_int_t;
-
-typedef struct tm             lf_tm_t;
+#include <sys/time.h>
 
 #define lf_tm_sec            tm_sec
 #define lf_tm_min            tm_min
@@ -27,40 +12,15 @@ typedef struct tm             lf_tm_t;
 #define lf_tm_wday           tm_wday
 #define lf_tm_isdst          tm_isdst
 
-#define lf_tm_sec_t          int
-#define lf_tm_min_t          int
-#define lf_tm_hour_t         int
-#define lf_tm_mday_t         int
-#define lf_tm_mon_t          int
-#define lf_tm_year_t         int
-#define lf_tm_wday_t         int
-
-
-#if (lf_HAVE_GMTOFF)
-#define lf_tm_gmtoff         tm_gmtoff
-#define lf_tm_zone           tm_zone
-#endif
-
-
-#if (lf_SOLARIS)
-
-#define lf_timezone(isdst) (- (isdst ? altzone : timezone) / 60)
-
-#else
-
-#define lf_timezone(isdst) (- (isdst ? timezone + 3600 : timezone) / 60)
-
-#endif
-
-
-void lf_timezone_update(void);
-void lf_localtime(time_t s, lf_tm_t *tm);
-void lf_libc_localtime(time_t s, struct tm *tm);
-void lf_libc_gmtime(time_t s, struct tm *tm);
+void lfGmTime(time_t s, struct tm *tm);
+void timeNow(time_t* nTime);
+void localTimeR(time_t s, struct tm *tm);
+void timeString(char strTime[80]);
 
 #define lf_gettimeofday(tp)  (void) gettimeofday(tp, NULL);
 #define lf_msleep(ms)        (void) usleep(ms * 1000)
 #define lf_sleep(s)          (void) sleep(s)
 
 
-#endif /* _lf_TIME_H_INCLUDED_ */
+#endif /* _lf_TIME_H_ */
+
