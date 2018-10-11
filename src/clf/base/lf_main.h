3e17c79ce57789ab2599608473286c08b9d251f4
pytf <43243160+pytf@users.noreply.github.com>
2018-10-11 19:57:03 +0800

    Update lf_main.h

--- src/clf/base/lf_main.h
+++ src/clf/base/lf_main.h
@@ -3,15 +3,22 @@
 
 #define mod_name_len 20
 #define LINEFEED             "\x0a"
+/* ´¥·¢½ø³Ì±ÀÀ£, ²úÉúcoredump raiseÏò½ø³Ì±¾Éí·¢ËÍÐÅºÅ*/
+#define BUG()  \
+    do { \
+        raise(SIGILL); \
+        exit(-1);      \
+    } while (0)
 
 typedef enum
 {
     MOD_ID_MAIN = 0,
-    MOD_ID_LOG,
-    MOD_ID_CONF,
-    MOD_ID_TASK,
-    MOD_ID_MSG,
-    MOD_ID_BUIT
+    MOD_ID_LOG  = 1,
+    MOD_ID_CONF  = 2,
+    MOD_ID_TASK  = 3,
+    MOD_ID_MSG  = 4,
+    MOD_ID_MONITOR = 5,
+    MOD_ID_BUIT = 16
 };
 
 typedef struct MOD_TABLE
@@ -21,4 +28,13 @@ typedef struct MOD_TABLE
     void *pFunc;
 }MOD_TABLE_S;
 
-#endif
+typedef struct MOD_CB_TABLE
+{
+    int mod_id;
+    char mod_name[mod_name_len];
+    int time_out;
+    void *pCBFunc;
+}MODULE_CB_TABLE_S;
+
+#endif /*__LF_MAIN_H__*/
+
